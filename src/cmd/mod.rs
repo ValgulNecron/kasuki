7209739cedbd6e21c209dfc9963a964974c3a121
valgul <valgulnecron@gmail.com>
2023-03-17 15:00:22 +0100

    yeah an another day

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -3,5 +3,4 @@ pub mod info;
 pub mod user;
 pub mod manga;
 pub mod ln;
-pub mod register_user;
 pub mod anime;
--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,6 @@ impl EventHandler for Handler {
                     .create_application_command(|command| cmd::manga::register(command))
                     .create_application_command(|command| cmd::ln::register(command))
                     .create_application_command(|command| cmd::user::register(command))
-                    .create_application_command(|command| cmd::register_user::register(command))
                     .create_application_command(|command| cmd::anime::register(command))
             }).await;
         println!("I created the following global slash command: {:#?}", guild_command);
@@ -50,10 +49,6 @@ impl EventHandler for Handler {
                 }
                 "user" => {
                     cmd::user::run(&command.data.options, &ctx, &command)
-                        .await
-                }
-                "register" => {
-                    cmd::register_user::run(&command.data.options, &ctx, &command)
                         .await
                 }
                 "anime" => {
