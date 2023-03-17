5024ea3e7e18a0baf43c07cf433ee3216d03407a
valgul <valgulnecron@gmail.com>
2023-03-17 15:10:33 +0100

    why there is 2 user

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,10 @@ impl EventHandler for Handler {
                 commands
                     .create_application_command(|command| cmd::ping::register(command))
                     .create_application_command(|command| cmd::info::register(command))
-                    .create_application_command(|command| cmd::user::register(command))
                     .create_application_command(|command| cmd::manga::register(command))
                     .create_application_command(|command| cmd::ln::register(command))
                     .create_application_command(|command| cmd::anime::register(command))
+                    .create_application_command(|command| cmd::user::register(command))
             }).await;
         println!("I created the following global slash command: {:#?}", guild_command);
     }
