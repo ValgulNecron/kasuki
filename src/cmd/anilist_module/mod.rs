8b0dcdea58796cd834904960ad8ca3dfbad7f750
valgul <valgulnecron@gmail.com>
2023-06-20 22:21:11 +0200

    changed code structure.

---
+++ src/cmd/anilist_module/mod.rs
@@ -0,0 +1,9 @@
+pub mod ping;
+pub mod info;
+pub mod user;
+pub mod manga;
+pub mod ln;
+pub mod anime;
+pub mod comparison;
+pub mod weeb_level;
+pub mod staff;
--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,9 +1,1 @@
-pub mod ping;
-pub mod info;
-pub mod user;
-pub mod manga;
-pub mod ln;
-pub mod anime;
-pub mod comparison;
-pub mod weeb_level;
-pub mod staff;
+pub mod anilist_module;
