156ae1c54bcce715982d9b9ebb6294bbdf6fdbe1
valgul <valgulnecron@gmail.com>
2023-07-08 20:49:16 +0200

    Not working. continued on compare.

--- src/cmd/anilist_module/compare.rs
+++ src/cmd/anilist_module/compare.rs
@@ -146,45 +146,45 @@ pub async fn embed(options: &[CommandDataOption], ctx: &Context, command: &Appli
 
     let anime_count_text;
     if user1.statistics.anime.count > user2.statistics.anime.count {
-        anime_count_text = format!("{} as more anime than {}", user_name1, user_name2)
+        anime_count_text = format!("{}{}{}", user_name1, user_name2)
     } else if user1.statistics.anime.count < user2.statistics.anime.count {
-        anime_count_text = format!("{} as more anime than {}", user_name2, user_name1)
+        anime_count_text = format!("{}{}}{}", user_name2, user_name1)
     } else {
-        anime_count_text = format!("{} and {} as the same amount of anime watched.", user_name1, user_name2)
+        anime_count_text = format!("{}{}{}{}", user_name1, user_name2)
     }
 
     let anime_watch_time;
     if user1.statistics.anime.minutes_watched > user2.statistics.anime.minutes_watched {
-        anime_watch_time = format!("{} as watched anime for longer than {}", user_name1, user_name2)
+        anime_watch_time = format!("{}{}{}", user_name1, user_name2)
     } else if user1.statistics.anime.minutes_watched < user2.statistics.anime.minutes_watched {
-        anime_watch_time = format!("{} as watched anime for longer than {}", user_name2, user_name1)
+        anime_watch_time = format!("{}{}}{}", user_name2, user_name1)
     } else {
-        anime_watch_time = format!("{} and {} as the same amount of anime watch time.", user_name1, user_name2)
+        anime_watch_time = format!("{}{}{}{}", user_name1, user_name2)
     }
 
     let manga_count_text;
     if user1.statistics.manga.count > user2.statistics.manga.count {
-        manga_count_text = format!("{} as read more manga than {}", user_name1, user_name2)
+        manga_count_text = format!("{}{}{}", user_name1, user_name2)
     } else if user1.statistics.manga.count < user2.statistics.manga.count {
-        manga_count_text = format!("{} as read more manga than {}", user_name2, user_name1)
+        manga_count_text = format!("{}{}}{}", user_name2, user_name1)
     } else {
-        manga_count_text = format!("{} and {} as the same amount of manga read.", user_name1, user_name2)
+        manga_count_text = format!("{}{}{}{}", user_name1, user_name2)
     }
 
     let manga_chapter_count;
     if user1.statistics.manga.chapters_read > user2.statistics.manga.chapters_read {
-        manga_chapter_count = format!("{} as read more chapter than {}", user_name1, user_name2)
+        manga_chapter_count = format!("{}{}{}", user_name1, user_name2)
     } else if user1.statistics.manga.chapters_read < user2.statistics.manga.chapters_read {
-        manga_chapter_count = format!("{} as read more chapter than {}", user_name2, user_name1)
+        manga_chapter_count = format!("{}{}{}", user_name2, user_name1)
     } else {
-        manga_chapter_count = format!("{} and {} as the same amount of chapter read.", user_name1, user_name2)
+        manga_chapter_count = format!("{}{}{}{}", user_name1, user_name2)
     }
 
     let pref_anime_genre1 = user1.statistics.anime.genres[0].clone().genre.unwrap();
     let pref_anime_genre2 = user2.statistics.anime.genres[0].clone().genre.unwrap();
     let pref_anime_genre_text;
     if pref_anime_genre1 == pref_anime_genre2 {
-        pref_anime_genre_text = format!("Both {} and {} prefer {} genre for anime.", user_name1, user_name2, pref_anime_genre1);
+        pref_anime_genre_text = format!("{} and {} prefer {} genre for anime.", user_name1, user_name2, pref_anime_genre1);
     } else {
         pref_anime_genre_text = format!("{} prefer {} while {} prefer {} for anime.", user_name1, pref_anime_genre1, user_name2, pref_anime_genre2);
     }
@@ -193,7 +193,7 @@ pub async fn embed(options: &[CommandDataOption], ctx: &Context, command: &Appli
     let pref_anime_tag2 = user2.statistics.anime.tags[0].clone().tag.name.unwrap();
     let pref_anime_tag_text;
     if pref_anime_tag1 == pref_anime_tag2 {
-        pref_anime_tag_text = format!("Both {} and {} prefer {} tag for anime.", user_name1, user_name2, pref_anime_tag1);
+        pref_anime_tag_text = format!("{} and {} prefer {} tag for anime.", user_name1, user_name2, pref_anime_tag1);
     } else {
         pref_anime_tag_text = format!("{} prefer {} while {} prefer {} for anime.", user_name1, pref_anime_tag1, user_name2, pref_anime_tag2);
     }
