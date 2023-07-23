36ab2a953e82c396a0c0be226f5c60290f9f2d5c
Valgul <valgulnecron@gmail.com>
2023-07-23 18:14:04 +0200

    Updated trim

--- src/cmd/general_module/trim.rs
+++ src/cmd/general_module/trim.rs
@@ -1,13 +1,12 @@
 pub fn trim(desc: String, lenght_diff: i32) -> String {
-    let count = desc.matches("||").count();
-    let desc_trim;
-    println!("{}", count);
-    if count % 2 == 0 {
-        let trim_length = desc.len() - ((lenght_diff * -1) as usize + 5);
-        desc_trim = format!("{}||...", &desc[..trim_length]);
-    } else {
-        let trim_length = desc.len() - ((lenght_diff * -1) as usize + 5);
-        desc_trim = format!("{}||...", &desc[..trim_length]);
+    let mut desc_trim;
+    let trim_length = desc.len() - ((lenght_diff * -1) as usize + 3);
+        desc_trim = format!("{}...", &desc[..trim_length]);
+
+    let count = desc_trim.matches("||").count();
+    if count % 2 != 0 {
+        let trim_length = desc.len() - ((lenght_diff * -2) as usize + 2);
+        desc_trim = format!("{}||..", &desc[..trim_length]);
     }
     return desc_trim;
 }
