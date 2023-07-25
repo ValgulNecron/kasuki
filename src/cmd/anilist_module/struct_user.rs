d960e1b33a090c651665a79af5760c21bccb595b
Valgul <valgulnecron@gmail.com>
2023-07-26 01:21:31 +0200

    Updated user struct and user to have reusable code.

--- src/cmd/anilist_module/struct_user.rs
+++ src/cmd/anilist_module/struct_user.rs
@@ -1,4 +1,7 @@
 use serde::Deserialize;
+use serenity::utils::Colour;
+
+use crate::cmd::general_module::lang_struct::UserLocalisedText;
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct UserWrapper {
@@ -95,5 +98,261 @@ pub fn resp_to_user_data(resp: String) -> Result<UserWrapper, String> {
             println!("Failed to parse JSON: {}", e);
             Err(String::from("Error: Failed to retrieve user data"))
         }
+    }
+}
+
+impl UserWrapper {
+    pub fn get_anime_genre(&self) -> String {
+        let mut anime_genre = String::new();
+        for i in 0..3 {
+            if let Some(genre) = self
+                .data
+                .user
+                .statistics
+                .anime
+                .genres
+                .get(i)
+                .and_then(|g| g.genre.as_ref())
+            {
+                anime_genre.push_str(&format!("{} / ", genre));
+            } else {
+                anime_genre.push_str("N/A / ");
+            }
+        }
+        anime_genre.pop();
+        anime_genre.pop();
+        anime_genre
+    }
+
+    pub fn get_anime_tag(&self) -> String{
+        let mut anime_tag_name = String::new();
+        for i in 0..3 {
+            if let Some(tags) = self
+                .data
+                .user
+                .statistics
+                .anime
+                .tags
+                .get(i)
+                .and_then(|g| g.tag.name.as_ref())
+            {
+                anime_tag_name.push_str(&format!("{} / ", tags));
+            } else {
+                anime_tag_name.push_str("N/A / ");
+            }
+        }
+        anime_tag_name.pop();
+        anime_tag_name.pop();
+        anime_tag_name
+    }
+
+    pub fn time_anime_watched(&self, localised_text: UserLocalisedText) -> String {
+        let mut min = self
+            .data
+            .user
+            .statistics
+            .anime
+            .minutes_watched
+            .unwrap_or_else(|| 0);
+        let mut hour = 0;
+        let mut days = 0;
+        let mut week = 0;
+
+        if min >= 60 {
+            hour = min / 60;
+            min = min % 60;
+        }
+
+        if hour >= 24 {
+            days = hour / 24;
+            hour = hour % 24;
+        }
+
+        if days >= 7 {
+            week = days / 7;
+            days = days % 7;
+        }
+
+        format!(
+            "{}{}{}{}{}{}{}{}",
+            week,
+            &localised_text.week,
+            days,
+            &localised_text.day,
+            hour,
+            &localised_text.hour,
+            min,
+            &localised_text.minute
+        )
+    }
+
+    pub fn get_anime_count(&self) -> i32 {
+        self.data.user.statistics.anime.count.unwrap_or_else(|| 0)
+    }
+
+    pub fn get_anime_score(&self) -> f64 {
+        self.data.user.statistics.anime.mean_score.unwrap_or_else(|| 0f64)
+    }
+
+    pub fn get_anime_standard_deviation(&self) -> f64 {
+        self.data.user.statistics.anime.standard_deviation.unwrap_or_else(|| 0f64)
+    }
+
+    pub fn get_anime_completed(&self) -> i32 {
+        let anime_statuses = &self.data.user.statistics.anime.statuses;
+        let mut anime_completed = 0;
+        for i in anime_statuses {
+            if i.status == "COMPLETED".to_string() {
+                anime_completed = i.count;
+            }
+        }
+        anime_completed
+    }
+
+
+    pub fn get_color(&self) -> Colour {
+        let mut _color = Colour::FABLED_PINK;
+        match self
+            .data
+            .user
+            .options
+            .profile_color
+            .clone()
+            .unwrap_or_else(|| "#FF00FF".to_string())
+            .as_str()
+        {
+            "blue" => _color = Colour::BLUE,
+            "purple" => _color = Colour::PURPLE,
+            "pink" => _color = Colour::MEIBE_PINK,
+            "orange" => _color = Colour::ORANGE,
+            "red" => _color = Colour::RED,
+            "green" => _color = Colour::DARK_GREEN,
+            "gray" => _color = Colour::LIGHT_GREY,
+            _ => {
+                _color = {
+                    let hex_code = "#0D966D";
+                    let color_code = u32::from_str_radix(&hex_code[1..], 16).unwrap();
+                    Colour::new(color_code)
+                }
+            }
+        }
+        _color
+    }
+
+    pub fn get_username(&self) -> String {
+        self.data.user.name.clone().unwrap_or_else(|| "N/A".to_string())
+    }
+
+    pub fn get_pfp(&self) -> String {
+        self.data.user.avatar.large.clone().unwrap_or_else(||
+            "https://imgs.search.brave.com/CYnhSvdQcm9aZe3wG84YY0B19zT2wlAuAkiAGu0mcLc/rs:fit:640:400:1/g:ce/aHR0cDovL3d3dy5m/cmVtb250Z3VyZHdh/cmEub3JnL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDIwLzA2L25v/LWltYWdlLWljb24t/Mi5wbmc"
+                .to_string())
+    }
+
+    pub fn get_banner(&self) -> String {
+        format!("https://img.anili.st/user/{}", self.data.user.id.unwrap())
+    }
+
+
+    pub fn get_manga_genre(&self) -> String {
+        let mut manga_genre = String::new();
+        for i in 0..3 {
+            if let Some(genre) = self
+                .data
+                .user
+                .statistics
+                .manga
+                .genres
+                .get(i)
+                .and_then(|g| g.genre.as_ref())
+            {
+                manga_genre.push_str(&format!("{} / ", genre));
+            } else {
+                manga_genre.push_str("N/A / ");
+            }
+        }
+        manga_genre.pop();
+        manga_genre.pop();
+        manga_genre
+    }
+
+    pub fn get_manga_tag(&self) -> String {
+        let mut manga_tag_name = String::new();
+        for i in 0..3 {
+            if let Some(tags) = self
+                .data
+                .user
+                .statistics
+                .manga
+                .tags
+                .get(i)
+                .and_then(|g| g.tag.name.as_ref())
+            {
+                manga_tag_name.push_str(&format!("{} / ", tags));
+            } else {
+                manga_tag_name.push_str("N/A / ");
+            }
+        }
+        manga_tag_name.pop();
+        manga_tag_name.pop();
+        manga_tag_name
+    }
+
+    pub fn get_manga_chapter(&self) -> i32 {
+        self.data
+            .user
+            .statistics
+            .manga
+            .chapters_read
+            .unwrap_or_else(|| 0)
+    }
+
+    pub fn get_manga_count(&self) -> i32 {
+        self.data.user.statistics.manga.count.unwrap_or_else(|| 0)
+    }
+
+    pub fn get_manga_score(&self) -> f64 {
+        self.data
+            .user
+            .statistics
+            .manga
+            .mean_score
+            .unwrap_or_else(|| 0f64)
+    }
+
+    pub fn get_manga_standard_deviation(&self) -> f64 {
+        self.data
+            .user
+            .statistics
+            .manga
+            .standard_deviation
+            .unwrap_or_else(|| 0f64)
+    }
+
+    pub fn get_manga_completed(&self) -> i32 {
+        let manga_statuses = &self.data.user.statistics.manga.statuses;
+        let mut manga_completed = 0;
+        for i in manga_statuses {
+            if i.status == "COMPLETED".to_string() {
+                manga_completed = i.count;
+            }
+        }
+        manga_completed
+    }
+
+
+    pub fn get_user_url(&self) -> String {
+        format!(
+            "https://anilist.co/user/{}",
+            self.data.user.id.unwrap_or_else(|| 1)
+        )
+    }
+
+    pub fn get_user_anime_url(&self) -> String {
+        format!("{}/animelist", self.get_user_url())
+    }
+
+    pub fn get_user_manga_url(&self) -> String {
+        format!("{}/mangalist", self.get_user_url())
     }
 }
--- src/cmd/anilist_module/user.rs
+++ src/cmd/anilist_module/user.rs
@@ -17,7 +17,6 @@ use serenity::model::Timestamp;
 use crate::cmd::anilist_module::struct_autocomplete::AutocompleteOption;
 use crate::cmd::anilist_module::struct_autocomplete_user::UserPageWrapper;
 use crate::cmd::anilist_module::struct_user::*;
-use crate::cmd::general_module::color::get_user_color;
 use crate::cmd::general_module::get_guild_langage::get_guild_langage;
 use crate::cmd::general_module::lang_struct::UserLocalisedText;
 use crate::cmd::general_module::pool::get_pool;
@@ -219,183 +218,33 @@ pub async fn embed(
     let lang_choice = get_guild_langage(guild_id).await;
 
     if let Some(localised_text) = json_data.get(lang_choice.as_str()) {
-        let user_url = format!(
-            "https://anilist.co/user/{}",
-            &data.data.user.id.unwrap_or_else(|| 1)
-        );
-        let color = get_user_color(data.clone());
+        let user_url = data.get_user_url();
 
-        let mut min = data
-            .data
-            .user
-            .statistics
-            .anime
-            .minutes_watched
-            .unwrap_or_else(|| 0);
-        let mut hour = 0;
-        let mut days = 0;
-        let mut week = 0;
+        let color = data.get_color();
 
-        if min >= 60 {
-            hour = min / 60;
-            min = min % 60;
-        }
+        let chap = data.get_manga_chapter();
+        let manga_genre = data.get_manga_genre();
+        let manga_count = data.get_manga_count();
+        let manga_score = data.get_manga_score();
+        let manga_standard_deviation = data.get_manga_standard_deviation();
+        let manga_tag_name = data.get_manga_tag();
+        let manga_completed = data.get_manga_completed();
 
-        if hour >= 24 {
-            days = hour / 24;
-            hour = hour % 24;
-        }
+        let time_watched = data.time_anime_watched(localised_text.clone());
 
-        if days >= 7 {
-            week = days / 7;
-            days = days % 7;
-        }
-        let chap = data
-            .data
-            .user
-            .statistics
-            .manga
-            .chapters_read
-            .unwrap_or_else(|| 0);
-        let time_watched = format!(
-            "{}{}{}{}{}{}{}{}",
-            week,
-            &localised_text.week,
-            days,
-            &localised_text.day,
-            hour,
-            &localised_text.hour,
-            min,
-            &localised_text.minute
-        );
-        let manga_count = data.data.user.statistics.manga.count.unwrap_or_else(|| 0);
-        let manga_score = data
-            .data
-            .user
-            .statistics
-            .manga
-            .mean_score
-            .unwrap_or_else(|| 0f64);
-        let manga_standard_deviation = data
-            .data
-            .user
-            .statistics
-            .manga
-            .standard_deviation
-            .unwrap_or_else(|| 0f64);
-        let mut manga_tag_name = String::new();
-        for i in 0..3 {
-            if let Some(tags) = data
-                .data
-                .user
-                .statistics
-                .manga
-                .tags
-                .get(i)
-                .and_then(|g| g.tag.name.as_ref())
-            {
-                manga_tag_name.push_str(&format!("{} / ", tags));
-            } else {
-                manga_tag_name.push_str("N/A / ");
-            }
-        }
-        manga_tag_name.pop();
-        manga_tag_name.pop();
+        let anime_count = data.get_anime_count();
+        let anime_score = data.get_anime_score();
+        let anime_standard_deviation = data.get_anime_standard_deviation();
+        let anime_tag_name = data.get_anime_tag();
+        let anime_genre = data.get_anime_genre();
+        let anime_completed = data.get_anime_completed();
 
-        let mut manga_genre = String::new();
-        for i in 0..3 {
-            if let Some(genre) = data
-                .data
-                .user
-                .statistics
-                .manga
-                .genres
-                .get(i)
-                .and_then(|g| g.genre.as_ref())
-            {
-                manga_genre.push_str(&format!("{} / ", genre));
-            } else {
-                manga_genre.push_str("N/A / ");
-            }
-        }
-        manga_genre.pop();
-        manga_genre.pop();
+        let manga_url = data.get_user_manga_url();
+        let anime_url = data.get_user_anime_url();
 
-        let anime_count = data.data.user.statistics.anime.count.unwrap_or_else(|| 0);
-        let anime_score = data
-            .data
-            .user
-            .statistics
-            .anime
-            .mean_score
-            .unwrap_or_else(|| 0f64);
-        let anime_standard_deviation = data
-            .data
-            .user
-            .statistics
-            .anime
-            .standard_deviation
-            .unwrap_or_else(|| 0f64);
-
-        let mut anime_tag_name = String::new();
-        for i in 0..3 {
-            if let Some(tags) = data
-                .data
-                .user
-                .statistics
-                .anime
-                .tags
-                .get(i)
-                .and_then(|g| g.tag.name.as_ref())
-            {
-                anime_tag_name.push_str(&format!("{} / ", tags));
-            } else {
-                anime_tag_name.push_str("N/A / ");
-            }
-        }
-        anime_tag_name.pop();
-        anime_tag_name.pop();
-
-        let mut anime_genre = String::new();
-        for i in 0..3 {
-            if let Some(genre) = data
-                .data
-                .user
-                .statistics
-                .anime
-                .genres
-                .get(i)
-                .and_then(|g| g.genre.as_ref())
-            {
-                anime_genre.push_str(&format!("{} / ", genre));
-            } else {
-                anime_genre.push_str("N/A / ");
-            }
-        }
-        anime_genre.pop();
-        anime_genre.pop();
-
-        let manga_url = format!("{}/mangalist", &user_url);
-        let anime_url = format!("{}/animelist", &user_url);
-
-        let anime_statuses = data.data.user.statistics.anime.statuses;
-        let mut anime_completed = 0;
-        for i in anime_statuses {
-            if i.status == "COMPLETED".to_string() {
-                anime_completed = i.count;
-            }
-        }
-
-        let manga_statuses = data.data.user.statistics.manga.statuses;
-        let mut manga_completed = 0;
-        for i in manga_statuses {
-            if i.status == "COMPLETED".to_string() {
-                manga_completed = i.count;
-            }
-        }
-        let user = data.data.user.name.unwrap_or_else(|| "N/A".to_string());
-        let profile_picture = data.data.user.avatar.large.unwrap_or_else(|| "https://imgs.search.brave.com/CYnhSvdQcm9aZe3wG84YY0B19zT2wlAuAkiAGu0mcLc/rs:fit:640:400:1/g:ce/aHR0cDovL3d3dy5m/cmVtb250Z3VyZHdh/cmEub3JnL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDIwLzA2L25v/LWltYWdlLWljb24t/Mi5wbmc".to_string());
-        let banner = format!("https://img.anili.st/user/{}", data.data.user.id.unwrap());
+        let user = data.get_username();
+        let profile_picture = data.get_pfp();
+        let banner = data.get_banner();
 
         if let Err(why) = command
             .create_interaction_response(&ctx.http, |response| {
