6fd271b8f930927253888b5487756ed7001d2784
valgul <valgulnecron@gmail.com>
2023-07-30 20:24:11 +0200

    Updated cache.db updated media.

--- src/cmd/anilist_module/command_media_ln.rs
+++ src/cmd/anilist_module/command_media_ln.rs
@@ -14,9 +14,7 @@ use serenity::utils::Colour;
 use crate::cmd::anilist_module::get_nsfw_channel::get_nsfw;
 use crate::cmd::anilist_module::struct_media::*;
 use crate::cmd::general_module::get_guild_langage::get_guild_langage;
-use crate::cmd::general_module::html_parser::convert_to_markdown;
 use crate::cmd::general_module::lang_struct::MediaLocalisedText;
-use crate::cmd::general_module::trim::trim;
 
 pub async fn embed(
     options: &[CommandDataOption],
@@ -98,104 +96,16 @@ pub async fn embed(
             }
 
             let banner_image = format!("https://img.anili.st/media/{}", data.data.media.id);
-            let mut desc = data
-                .data
-                .media
-                .description
-                .unwrap_or_else(|| "NA".to_string());
-            let en_name = data
-                .data
-                .media
-                .title
-                .english
-                .unwrap_or_else(|| "NA".to_string());
-            let rj_name = data
-                .data
-                .media
-                .title
-                .romaji
-                .unwrap_or_else(|| "NA".to_string());
-            let thumbnail = data.data.media.cover_image.extra_large.unwrap_or_else(|| "https://imgs.search.brave.com/CYnhSvdQcm9aZe3wG84YY0B19zT2wlAuAkiAGu0mcLc/rs:fit:640:400:1/g:ce/aHR0cDovL3d3dy5m/cmVtb250Z3VyZHdh/cmEub3JnL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDIwLzA2L25v/LWltYWdlLWljb24t/Mi5wbmc".to_string());
-            let site_url = data
-                .data
-                .media
-                .site_url
-                .unwrap_or_else(|| "https://example.com".to_string());
-            let name = format!("{} / {}", en_name, rj_name);
-            let format = data.data.media.format.unwrap_or_else(|| "N/A".to_string());
-            let source = data.data.media.source.unwrap_or_else(|| "N/A".to_string());
+            let desc = data.get_desc();
+            let thumbnail = data.get_thumbnail();
+            let site_url = data.get_url();
+            let name = data.get_name();
 
-            let start_y = data.data.media.start_date.year.unwrap_or_else(|| 0);
-            let start_d = data.data.media.start_date.day.unwrap_or_else(|| 0);
-            let start_m = data.data.media.start_date.month.unwrap_or_else(|| 0);
-            let start_date = if start_y == 0 && start_d == 0 && start_m == 0 {
-                "N/A".to_string()
-            } else {
-                format!("{}/{}/{}", start_d, start_m, start_y)
-            };
-            let end_y = data.data.media.end_date.year.unwrap_or_else(|| 0);
-            let end_d = data.data.media.end_date.day.unwrap_or_else(|| 0);
-            let end_m = data.data.media.end_date.month.unwrap_or_else(|| 0);
-            let end_date = if end_y == 0 && end_d == 0 && end_m == 0 {
-                "N/A".to_string()
-            } else {
-                format!("{}/{}/{}", start_d, start_m, start_y)
-            };
-
-            let mut staff = "".to_string();
-            let staffs = data.data.media.staff.edges;
-            for s in staffs {
-                let full = s.node.name.full.unwrap_or_else(|| "N/A".to_string());
-                let user = s
-                    .node
-                    .name
-                    .user_preferred
-                    .unwrap_or_else(|| "N/A".to_string());
-                let role = s.role.unwrap_or_else(|| "N/A".to_string());
-                staff.push_str(&format!(
-                    "{}{}{}{}{}{}\n",
-                    &localised_text.full_name,
-                    full,
-                    &localised_text.user_pref,
-                    user,
-                    &localised_text.role,
-                    role
-                ));
-            }
-
-            let info = format!(
-                "{}{}{}{}{}{}{}{} \n {}",
-                &localised_text.format,
-                format,
-                &localised_text.source,
-                source,
-                &localised_text.start_date,
-                start_date,
-                &localised_text.end_date,
-                end_date,
-                staff
-            );
-            let mut genre = "".to_string();
-            let genre_list = data.data.media.genres;
-            for g in genre_list {
-                genre += &g.unwrap_or_else(|| "N/A".to_string());
-                genre += "\n"
-            }
-            let mut tag = "".to_string();
-            let tag_list = data.data.media.tags;
-            for t in tag_list.iter().take(10) {
-                let tag_name: String = t.name.as_ref().map_or("N/A".to_string(), |s| s.to_string());
-                tag += &tag_name;
-                tag += "\n";
-            }
+            let info = data.get_media_info(localised_text.clone());
+            let genre = data.get_genres();
+            let tag = data.get_tags();
 
             let color = Colour::FABLED_PINK;
-
-            desc = convert_to_markdown(desc);
-            let lenght_diff = 4096 - desc.len() as i32;
-            if lenght_diff <= 0 {
-                desc = trim(desc, lenght_diff)
-            }
 
             if let Err(why) = command
                 .create_interaction_response(&ctx.http, |response| {
--- src/cmd/anilist_module/struct_media.rs
+++ src/cmd/anilist_module/struct_media.rs
@@ -679,6 +679,31 @@ impl MediaWrapper {
         staff
     }
 
+    pub fn get_media_staff(&self, localised_text: MediaLocalisedText) -> String {
+        let mut staff = "".to_string();
+            let staffs = &self.data.media.staff.edges;
+            for s in staffs {
+                let full = s.node.name.full.clone().unwrap_or_else(|| "N/A".to_string());
+                let user = s
+                    .node
+                    .name
+                    .user_preferred
+                    .clone()
+                    .unwrap_or_else(|| "N/A".to_string());
+                let role = s.role.clone().unwrap_or_else(|| "N/A".to_string());
+                staff.push_str(&format!(
+                    "{}{}{}{}{}{}\n",
+                    &localised_text.full_name,
+                    full,
+                    &localised_text.user_pref,
+                    user,
+                    &localised_text.role,
+                    role
+                ));
+            }
+        staff
+    }
+
     pub fn get_anime_info(&self, localised_text: AnimeLocalisedText) -> String {
         let format = self.get_format();
         let source = self.get_source();
@@ -698,6 +723,26 @@ impl MediaWrapper {
             end_date,
             staff
         )
+    }
+
+    pub fn get_media_info(&self, localised_text: MediaLocalisedText) -> String {
+        let format = self.get_format();
+            let source = self.get_source();
+            let start_date = self.get_start_date();
+            let end_date = self.get_end_date();
+            let staff = self.get_media_staff(localised_text.clone());
+        format!(
+                "{}{}{}{}{}{}{}{} \n {}",
+                &localised_text.format,
+                format,
+                &localised_text.source,
+                source,
+                &localised_text.start_date,
+                start_date,
+                &localised_text.end_date,
+                end_date,
+                staff
+            )
     }
 
     pub fn get_genres(&self) -> String {
