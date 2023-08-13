38f04feb6c3b6bb6d9dd239a86ab7fadd275c81d
valgul <valgulnecron@gmail.com>
2023-08-13 22:40:21 +0200

    Updated expect

--- src/cmd/ai_module/transcript.rs
+++ src/cmd/ai_module/transcript.rs
@@ -145,7 +145,7 @@ pub async fn run(
             let path = Path::new(my_path);
             let _ = dotenv::from_path(path);
             let api_key = env::var("AI_API_TOKEN").expect("token");
-            let api_base_url = env::var("AI_API_BASE_URL").expect("token");
+            let api_base_url = env::var("AI_API_BASE_URL").expect("base url");
             let api_url = format!("{}audio/transcriptions", api_base_url);
             let client = reqwest::Client::new();
             let mut headers = HeaderMap::new();
