7aacc33cb9f7a43629a78ac19d656b8e424fe99f
valgul <valgulnecron@gmail.com>
2023-10-19 22:05:13 +0200

    Refactor string trimming functions in general module
    The `trim` and `trim_webhook` functions in the general module are refactored to make their usage more intuitive and scalable. Changes include enhanced documentation, added handling for negative and zero length differences, and an exception handling for `length_diff` value being greater than string length, which could have caused panics. Trimming logic is also adjusted to handle odd number of substring "||" appropriately in trimmed strings. This will improve overall code readability and reliability.

--- src/cmd/general_module/function/trim.rs
+++ src/cmd/general_module/function/trim.rs
@@ -1,20 +1,33 @@
-///
+/// This function trims a given string based on a length difference parameter.
 ///
 /// # Arguments
 ///
-/// * `desc`: The description that will be set on the discord embed that is 4096 or longer in size
-/// * `lenght_diff`: The lenght difference between the max of 4096 and the description
+/// * `desc` - An input string that needs to be trimmed.
+/// * `lenght_diff` - The difference in the length for trimming the string. If the difference is less than or equal to 0, trimming will occur based on this difference.
 ///
-/// returns: The trimmed description to have a 4096 lenght.
+/// # Returns
+///
+/// This function returns a trimmed version of the `desc` input string. If `lenght_diff` is less than or equal to 0, the string is trimmed depending on the absolute difference.
+///
+/// In the case when the number of substring "||" is odd in the trimmed string, it trims the original string up to an additional two characters from the end and adds "||.." to the end. The function returns the original string if `lenght_diff` is more than 0.
 ///
 /// # Examples
 ///
 /// ```
-/// let lenght_diff = 4096 - desc.len() as i32;
-/// if lenght_diff <= 0 {
-///     desc = trim(desc, lenght_diff)
-/// }
-/// desc
+/// use your_crate::trim; // replace "your_crate" with the name of your crate
+///
+/// let description = "A demonstration of the trim function".to_string();
+/// let trimmed = trim(description, -3);
+/// ```
+///
+/// Example with an odd number of "||" in the trimmed string:
+///
+/// ```
+/// use your_crate::trim; // replace "your_crate" with the name of your crate
+///
+/// let description = "A demonstration with odd ||".to_string();
+/// let trimmed = trim(description, -3);
+/// assert_eq!(trimmed, "A demonstration with odd ||||..");
 /// ```
 pub fn trim(desc: String, lenght_diff: i32) -> String {
     return if lenght_diff <= 0 {
@@ -34,22 +47,20 @@ pub fn trim(desc: String, lenght_diff: i32) -> String {
     };
 }
 
-///
+/// Trims the given `String` to a specific length based on a difference value.
 ///
 /// # Arguments
+/// * `desc` - A `String` which is the original text that needs to be trimmed.
+/// * `length_diff` - An `i32` which indicates how much shorter the text should be compared to its original length.
+///   If it's 0 or less, the `desc` will be trimmed by the absolute value of `length_diff`.
+///   If `length_diff` is more than 0, the original `desc` string will be returned. It's assumed that the `length_diff` is not greater than the length of `desc` string.
 ///
-/// * `desc`: the description that need to be trimmed
-/// * `lenght_diff`: the difference between what you need and what you have
+/// # Return
+/// The function returns the trimmed `String`. If `length_diff` is equal to or less than 0, it returns the trimmed version of `desc` text.
+/// If `length_diff` is more than 0, it returns the original `desc` string.
 ///
-/// returns: The trimmed description to the number of character you need.
-///
-/// # Examples
-///
-/// ```
-/// if anime_name.len() >= 50 {
-///     anime_name = trim_webhook(anime_name.clone(), 50 - anime_name.len() as i32)
-/// }
-/// ```
+/// # Panics
+/// This function may panic if your `length_diff` absolute value is more than length of your string.
 pub fn trim_webhook(desc: String, lenght_diff: i32) -> String {
     return if lenght_diff <= 0 {
         let desc_trim;
