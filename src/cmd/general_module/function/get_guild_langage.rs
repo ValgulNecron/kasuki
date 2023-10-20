a0b415eb9abd0cdce68725fed02fdff756329555
valgulnecron <valgulnecron@gmail.com>
2023-10-20 12:13:10 +0200

    Refactor code and enhance code documentation.
    The code was refactored to increase readability and maintainability by simplifying the functions in pool.rs, html_parser.rs, and in_progress.rs to return their results directly, without unnecessary variable assignment. Html_parser.rs was also extensively expanded with in-depth documentation, giving clear breakdowns of the purpose, parameters, returns, and examples of usage for the functions. The added documentation is aimed at facilitating smoother development and review processes.

--- src/cmd/general_module/function/get_guild_langage.rs
+++ src/cmd/general_module/function/get_guild_langage.rs
@@ -1,5 +1,38 @@
 use crate::cmd::general_module::function::pool::get_pool;
 
+/// Asynchronously fetches the language of a given guild from the database.
+///
+/// This function takes the guild_id as an argument and returns the language associated with that guild.
+/// The function attempts to connect to the data.db database, then executes a SQL query to select
+/// the language of the given guild from the `guild_lang` table. If the given guild_id is not found
+/// in the database or if there's any issue with the query, the function will still return a default
+/// language of "En".
+///
+/// # Arguments
+///
+/// * `guild_id` - A string slice that holds the id of the guild.
+///
+/// # Examples
+///
+/// ```
+/// let guild_id = "some id";
+/// let guild_language = get_guild_langage(guild_id).await;
+/// assert_eq!(guild_language, "En");
+/// ```
+///
+/// # Returns
+///
+/// * `String` - The language of the guild. Returns "En" if no language is found for the guild.
+///
+/// # Panics
+///
+/// The function will panic if the database connection cannot be established or the SQL query
+/// execution fails.
+///
+/// # Errors
+///
+/// This function will return a tuple (None, None) if there is any error trying to fetch the guild
+/// language from the database.
 pub async fn get_guild_langage(guild_id: String) -> String {
     let database_url = "./data.db";
     let pool = get_pool(database_url).await;
