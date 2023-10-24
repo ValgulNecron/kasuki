bfd12a51b0dbf8a1c664d76a00e4757470b25163
valgul <valgulnecron@gmail.com>
2023-10-24 14:10:49 +0200

    Add avatar command and language file
    This commit introduces a new 'avatar' command that displays a user's avatar. Additionally, a new 'avatar.json' language file has been created. Previously we had 'profile' command which showed the profile of the user, now we changed it to ensure our command shows the avatar of the user instead. This enhances the bot's utility and makes user avatars more accessible to other users in the server.

--- src/cmd/general_module/cmd/avatar.rs
+++ src/cmd/general_module/cmd/avatar.rs
@@ -1,16 +1,38 @@
 use crate::cmd::lang_struct::register::general::struct_avatar_register::RegisterLocalisedAvatar;
 use serenity::builder::CreateApplicationCommand;
+use serenity::client::Context;
 use serenity::model::application::command::CommandOptionType;
+use serenity::model::prelude::application_command::{
+    ApplicationCommandInteraction, CommandDataOption, CommandDataOptionValue,
+};
+use serenity::model::prelude::User;
+
+pub async fn run(
+    options: &[CommandDataOption],
+    ctx: &Context,
+    command: &ApplicationCommandInteraction,
+) {
+    if let Some(option) = options.get(0) {
+        let resolved = option.resolved.as_ref().unwrap();
+        if let CommandDataOptionValue::User(user, ..) = resolved {
+            avatar_with_user(ctx, command, user).await
+        } else {
+            avatar_without_user(ctx, command).await
+        }
+    } else {
+        avatar_without_user(ctx, command).await
+    }
+}
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
     let profiles = RegisterLocalisedAvatar::get_avatar_register_localised().unwrap();
     let command = command
-        .name("profile")
-        .description("Show the profile of a user")
+        .name("avatar")
+        .description("Show the avatar of a user")
         .create_option(|option| {
             let option = option
                 .name("user")
-                .description("The user you wan the profile of")
+                .description("The user you wan the avatar of")
                 .kind(CommandOptionType::User)
                 .required(false);
             for profile in profiles.values() {
@@ -27,3 +49,7 @@ pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicatio
     }
     command
 }
+
+fn avatar_without_user(ctx: &Context, command: &ApplicationCommandInteraction) {}
+
+fn avatar_with_user(ctx: &Context, command: &ApplicationCommandInteraction, user: &User) {}
