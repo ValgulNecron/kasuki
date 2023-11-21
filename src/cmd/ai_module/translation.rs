676291b01c7e24801c9021e4cab706731124b053
valgulnecron <valgulnecron@gmail.com>
2023-11-21 11:53:57 +0100

    Handle 'None' case in translation.rs
    Modified the handling of attachment in 'src/cmd/ai_module/translation.rs'. Added a match clause to return early when attachment is 'None', which avoids the unnecessary execution of code in this case. This makes the code more efficient and easier to read.

--- src/cmd/ai_module/translation.rs
+++ src/cmd/ai_module/translation.rs
@@ -53,122 +53,124 @@ pub async fn run(
             }
         }
     }
+    let attachement = match attachement {
+        Some(a) => a,
+        None => return,
+    };
 
-    if let CommandDataOptionValue::Attachment(attachement) = attachement_option {
-        let localised_text =
-            match TranslationLocalisedText::get_translation_localised(ctx, command).await {
-                Ok(data) => data,
-                Err(_) => return,
-            };
-        let content_type = attachement.content_type.clone().unwrap();
-        let content = attachement.proxy_url.clone();
+    let localised_text =
+        match TranslationLocalisedText::get_translation_localised(ctx, command).await {
+            Ok(data) => data,
+            Err(_) => return,
+        };
+    let content_type = attachement.content_type.clone().unwrap();
+    let content = attachement.proxy_url.clone();
 
-        if !content_type.starts_with("audio/") && !content_type.starts_with("video/") {
-            error_file_type(ctx, command).await;
+    if !content_type.starts_with("audio/") && !content_type.starts_with("video/") {
+        error_file_type(ctx, command).await;
+        return;
+    }
+
+    let allowed_extensions = ["mp3", "mp4", "mpeg", "mpga", "m4a", "wav", "webm"];
+    let parsed_url = Url::parse(&content).expect("Failed to parse URL");
+    let path_segments = parsed_url
+        .path_segments()
+        .expect("Failed to retrieve path segments");
+    let last_segment = path_segments.last().expect("URL has no path segments");
+
+    let file_extension = last_segment
+        .rsplit('.')
+        .next()
+        .expect("No file extension found")
+        .to_lowercase();
+
+    if !allowed_extensions.contains(&&*file_extension) {
+        error_file_extension(ctx, command).await;
+        return;
+    }
+
+    let response = reqwest::get(content).await.expect("download");
+
+    let uuid_name = Uuid::new_v4();
+    let fname = Path::new("./").join(format!("{}.{}", uuid_name, file_extension));
+    let file_name = format!("/{}.{}", uuid_name, file_extension);
+    let mut file = File::create(fname.clone()).expect("file name");
+    let resp_byte = response.bytes().await.unwrap();
+    copy(&mut resp_byte.as_ref(), &mut file).unwrap();
+    let file_to_delete = fname.clone();
+
+    differed_response_with_file_deletion(ctx, command, file_to_delete.clone()).await;
+
+    let message: Message = match in_progress_embed(ctx, command).await {
+        Ok(Some(message_option)) => message_option,
+        Ok(None) => {
+            error_resolving_value_followup(ctx, command).await;
             return;
         }
-
-        let allowed_extensions = ["mp3", "mp4", "mpeg", "mpga", "m4a", "wav", "webm"];
-        let parsed_url = Url::parse(&content).expect("Failed to parse URL");
-        let path_segments = parsed_url
-            .path_segments()
-            .expect("Failed to retrieve path segments");
-        let last_segment = path_segments.last().expect("URL has no path segments");
-
-        let file_extension = last_segment
-            .rsplit('.')
-            .next()
-            .expect("No file extension found")
-            .to_lowercase();
-
-        if !allowed_extensions.contains(&&*file_extension) {
-            error_file_extension(ctx, command).await;
+        Err(error) => {
+            println!("Error: {}", error);
             return;
         }
+    };
 
-        let response = reqwest::get(content).await.expect("download");
+    let my_path = "./.env";
+    let path = Path::new(my_path);
+    let _ = dotenv::from_path(path);
+    let api_key = env::var("AI_API_TOKEN").expect("token");
+    let api_base_url = env::var("AI_API_BASE_URL").expect("base url");
+    let api_url = format!("{}audio/translations", api_base_url);
+    let client = reqwest::Client::new();
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        AUTHORIZATION,
+        HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap(),
+    );
 
-        let uuid_name = Uuid::new_v4();
-        let fname = Path::new("./").join(format!("{}.{}", uuid_name, file_extension));
-        let file_name = format!("/{}.{}", uuid_name, file_extension);
-        let mut file = File::create(fname.clone()).expect("file name");
-        let resp_byte = response.bytes().await.unwrap();
-        copy(&mut resp_byte.as_ref(), &mut file).unwrap();
-        let file_to_delete = fname.clone();
+    let file = fs::read(fname).unwrap();
+    let part = multipart::Part::bytes(file)
+        .file_name(file_name)
+        .mime_str(&content_type)
+        .unwrap();
+    let form = multipart::Form::new()
+        .part("file", part)
+        .text("model", "whisper-1")
+        .text("response_format", "json");
 
-        differed_response_with_file_deletion(ctx, command, file_to_delete.clone()).await;
-
-        let message: Message = match in_progress_embed(ctx, command).await {
-            Ok(Some(message_option)) => message_option,
-            Ok(None) => {
-                error_resolving_value_followup(ctx, command).await;
-                return;
-            }
-            Err(error) => {
-                println!("Error: {}", error);
-                return;
-            }
-        };
-
-        let my_path = "./.env";
-        let path = Path::new(my_path);
-        let _ = dotenv::from_path(path);
-        let api_key = env::var("AI_API_TOKEN").expect("token");
-        let api_base_url = env::var("AI_API_BASE_URL").expect("base url");
-        let api_url = format!("{}audio/translations", api_base_url);
-        let client = reqwest::Client::new();
-        let mut headers = HeaderMap::new();
-        headers.insert(
-            AUTHORIZATION,
-            HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap(),
-        );
-
-        let file = fs::read(fname).unwrap();
-        let part = multipart::Part::bytes(file)
-            .file_name(file_name)
-            .mime_str(&content_type)
-            .unwrap();
-        let form = multipart::Form::new()
-            .part("file", part)
-            .text("model", "whisper-1")
-            .text("response_format", "json");
-
-        let response_result = client
-            .post(api_url)
-            .headers(headers)
-            .multipart(form)
-            .send()
-            .await;
-        let response = match response_result {
-            Ok(res) => res,
-            Err(err) => {
-                let _ = fs::remove_file(&file_to_delete);
-                eprintln!("Error sending the requests: {}", err);
-                error_making_request_edit(ctx, command, message).await;
-                return;
-            }
-        };
-        println!("{:?}", response);
-        let res_result: Result<Value, reqwest::Error> = response.json().await;
-
-        let res = match res_result {
-            Ok(value) => value,
-            Err(err) => {
-                let _ = fs::remove_file(&file_to_delete);
-                eprintln!("Error parsing response as JSON: {}", err);
-                error_parsing_json_edit(ctx, message, command).await;
-                return;
-            }
-        };
-
-        let text = res["text"].as_str().unwrap_or("");
-        let _ = fs::remove_file(&file_to_delete);
-        if lang != "en" {
-            let text = translation(lang, text.to_string(), api_key, api_base_url).await;
-            translation_embed(ctx, text, message, localised_text.clone()).await;
-        } else {
-            translation_embed(ctx, text.to_string(), message, localised_text.clone()).await;
+    let response_result = client
+        .post(api_url)
+        .headers(headers)
+        .multipart(form)
+        .send()
+        .await;
+    let response = match response_result {
+        Ok(res) => res,
+        Err(err) => {
+            let _ = fs::remove_file(&file_to_delete);
+            eprintln!("Error sending the requests: {}", err);
+            error_making_request_edit(ctx, command, message).await;
+            return;
         }
+    };
+    println!("{:?}", response);
+    let res_result: Result<Value, reqwest::Error> = response.json().await;
+
+    let res = match res_result {
+        Ok(value) => value,
+        Err(err) => {
+            let _ = fs::remove_file(&file_to_delete);
+            eprintln!("Error parsing response as JSON: {}", err);
+            error_parsing_json_edit(ctx, message, command).await;
+            return;
+        }
+    };
+
+    let text = res["text"].as_str().unwrap_or("");
+    let _ = fs::remove_file(&file_to_delete);
+    if lang != "en" {
+        let text = translation(lang, text.to_string(), api_key, api_base_url).await;
+        translation_embed(ctx, text, message, localised_text.clone()).await;
+    } else {
+        translation_embed(ctx, text.to_string(), message, localised_text.clone()).await;
     }
 }
 
