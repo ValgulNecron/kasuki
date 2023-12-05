51cd42e11d0d889f7fa908371fdfb161dd5e5fe5
valgul <valgulnecron@gmail.com>
2023-12-05 19:14:50 +0100

    Managed to do it thank to ev3nvy

--- src/command_run/ai/transcript.rs
+++ src/command_run/ai/transcript.rs
@@ -3,33 +3,37 @@ use crate::error_enum::AppError;
 use crate::error_enum::AppError::NoCommandOption;
 use serenity::all::{
     Attachment, CommandDataOption, CommandDataOptionValue, CommandInteraction, Context,
+    ResolvedOption, ResolvedValue,
 };
 
 pub async fn run(
-    options: &[CommandDataOption],
+    options: &[ResolvedOption<'_>],
     ctx: &Context,
     command: &CommandInteraction,
 ) -> Result<(), AppError> {
     let mut prompt: String = String::new();
     let mut lang: String = String::new();
     let mut attachement: Option<Attachment> = None;
-    for option in options {
+    for option in options.to_owned() {
         if option.name == "lang_struct" {
-            let resolved = &option.value;
-            if let CommandDataOptionValue::String(lang_option) = resolved {
-                lang = lang_option.clone()
+            let resolved = option.value.clone();
+            if let ResolvedValue::String(lang_option) = resolved {
+                lang = String::from(lang_option)
             }
         }
         if option.name == "prompt" {
-            let resolved = &option.value;
-            if let CommandDataOptionValue::String(prompt_option) = resolved {
-                prompt = prompt_option.clone()
+            let resolved = option.value.clone();
+            if let ResolvedValue::String(prompt_option) = resolved {
+                prompt = String::from(prompt_option)
             }
         }
         if option.name == "video" {
-            let resolved = &option.value;
-            if let CommandDataOptionValue::Attachment(attachement_option) = resolved {
-                // attachement = ;
+            if let ResolvedOption {
+                value: ResolvedValue::Attachment(attachment_option),
+                ..
+            } = option
+            {
+                attachement = Some(attachment_option.to_owned())
             } else {
                 return Err(NoCommandOption(String::from(
                     "The command contain no option.",
