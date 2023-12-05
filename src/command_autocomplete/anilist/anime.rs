ab69a6535126f7ca7847b9b1b90e9a59c6d89efc
valgulnecron <valgulnecron@gmail.com>
2023-12-05 16:32:57 +0100

    Update autocomplete function for Anilist anime search
    Refactored the autocomplete function in src/command_autocomplete/anilist/anime.rs to manually create and send HTTP request for autocomplete interactions. This was done to explicitly set interaction attributes such as 'focused' for choices, 'type', 'id', and 'version' for interaction data. This gives us more control and flexibility over interaction responses sent to Discord.

--- src/command_autocomplete/anilist/anime.rs
+++ src/command_autocomplete/anilist/anime.rs
@@ -1,8 +1,12 @@
 use crate::anilist_struct::autocomplete::media::MediaPageWrapper;
+use reqwest::Client;
+use serde_json::json;
 use serenity::all::{AutocompleteChoice, CommandInteraction, Context, Interaction};
 use serenity::builder::CreateAutocompleteResponse;
+use serenity::http::Request;
 
 pub async fn autocomplete(ctx: Context, interaction: Interaction, command: CommandInteraction) {
+    let client = Client::new();
     let search = &command
         .data
         .options
@@ -19,20 +23,39 @@ pub async fn autocomplete(ctx: Context, interaction: Interaction, command: Comma
         let english = title_data.english;
         let romaji = title_data.romaji;
         let title = english.unwrap_or(romaji);
-        choices.push(AutocompleteChoice::new(title, data.id))
+        choices.push(json!({
+            "type": 3,
+            "name": title,
+            "value": data.id,
+            "focused": true,
+        }));
     }
-
-    let builder = CreateAutocompleteResponse::new().set_choices(choices);
     let interaction_id = interaction.id();
     let interaction_token = interaction.token();
-    let test = ctx
-        .http
-        .create_interaction_response(
-            interaction_id,
-            interaction_token,
-            (&builder).into(),
-            Vec::new(),
-        )
-        .await;
-    println!("{:?}", test);
+    let interaction_version = command.version;
+    let interaction_name = command.data.name.as_str();
+
+    let autocomplete_interaction = json!({
+      "type": 4,
+      "data": {
+          "id": interaction_id,
+          "name": interaction_name,
+          "type": 1,
+          "version": interaction_version,
+          "options": choices
+      }
+    });
+
+    let mut request = Request::new("POST");
+    request.set_url(
+        "https://discord.com/api/v8/interactions/{}/{}/callback",
+        interaction_id,
+        interaction_token,
+    );
+    request.set_body(&json!(autocomplete_interaction));
+
+    match response {
+        Ok(_) => println!("Autocomplete response sent successfully"),
+        Err(e) => println!("Failed to send autocomplete response: {}", e),
+    }
 }
