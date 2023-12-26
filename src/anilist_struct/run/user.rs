c523b1b862914ebe663b93667716e49fbbe4b764
valgul <valgulnecron@gmail.com>
2023-12-26 22:43:49 +0100

    Clippy

--- src/anilist_struct/run/user.rs
+++ src/anilist_struct/run/user.rs
@@ -454,28 +454,25 @@ fn get_anime_time_watch(i: i32, localised1: UserLocalised) -> String {
 }
 
 pub fn get_color(user: User) -> Colour {
-    let color;
-    match user
+    let color = match user
         .options
         .profile_color
         .clone()
         .unwrap_or_else(|| "#FF00FF".to_string())
         .as_str()
     {
-        "blue" => color = Colour::BLUE,
-        "purple" => color = Colour::PURPLE,
-        "pink" => color = Colour::MEIBE_PINK,
-        "orange" => color = Colour::ORANGE,
-        "red" => color = Colour::RED,
-        "green" => color = Colour::DARK_GREEN,
-        "gray" => color = Colour::LIGHT_GREY,
+        "blue" => Colour::BLUE,
+        "purple" => Colour::PURPLE,
+        "pink" => Colour::MEIBE_PINK,
+        "orange" => Colour::ORANGE,
+        "red" => Colour::RED,
+        "green" => Colour::DARK_GREEN,
+        "gray" => Colour::LIGHT_GREY,
         _ => {
-            color = {
-                let hex_code = "#0D966D";
-                let color_code = u32::from_str_radix(&hex_code[1..], 16).unwrap();
-                Colour::new(color_code)
-            }
+            let hex_code = "#0D966D";
+            let color_code = u32::from_str_radix(&hex_code[1..], 16).unwrap();
+            Colour::new(color_code)
         }
-    }
+    };
     color
 }
