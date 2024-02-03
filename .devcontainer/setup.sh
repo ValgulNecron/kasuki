fd0d1180160c599afae75a86d9b0f9984d6f454c
Valgul <valgulnecron@gmail.com>
2024-02-03 18:29:56 +0100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,35 +1,39 @@
 ## update and install some things we should probably have
 apt-get update
 apt-get install -y \
-  curl \
-  git \
-  gnupg2 \
-  jq \
-  sudo \
-  zsh \
-  vim \
-  build-essential \
-  openssl \
-  libssl-dev \
-  libsqlite3-dev \
-  libpng-dev \
-  libjpeg-dev \
-  ca-certificates
-        
+ curl \
+ git \
+ gnupg2 \
+ jq \
+ sudo \
+ zsh \
+ vim \
+ build-essential \
+ openssl \
+ libssl-dev \
+ libsqlite3-dev \
+ libpng-dev \
+ libjpeg-dev \
+ ca-certificates
 
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y 
-rustup install nightly
+rustup install stable
 rustup component add rustfmt
-rustup component add rustfmt --toolchain nightly
+rustup component add rustfmt --toolchain stable
 rustup component add clippy 
-rustup component add clippy --toolchain nightly
+rustup component add clippy --toolchain stable
 
 cargo install cargo-expand
 cargo install cargo-edit
 
 ## setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ZSH=$HOME/.oh-my-zsh
+ZSH_THEME="awesomepanda"
+sed -i -e "s/ZSH_THEME=.*/ZSH_THEME=\"awesomepanda\"/g" $ZSH/ohmyzsh.sh
+plugins=(git zsh-interactive-cd rust)
+sed -i -e "s/plugins=\(.*\)/plugins=(\1 git zsh-interactive-cd rust)/g" $ZSH/ohmyzsh.sh
 cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
