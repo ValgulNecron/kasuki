4fbc359c563e537f40cddf3bc2013e6ebcb41f91
valgul <valgulnecron@gmail.com>
2024-02-05 22:19:59 +0100

    Fixed some issue.

--- src/new_member.rs
+++ src/new_member.rs
@@ -23,86 +23,75 @@ pub async fn new_member(ctx: Context, member: &mut Member) -> Result<(), AppErro
     let fip = format!("{}/{}.webp", SERVER_IMAGE_PATH, member.guild_id);
     let full_image_path = fip.as_str();
 
-    if Path::new(full_image_path).exists() {
-        let guild = member
-            .guild_id
-            .to_partial_guild_with_counts(&ctx.http)
-            .await
-            .map_err(|e| {
-                NewMemberError(NewMemberErrorOptionError(format!(
-                    "there was an error getting the guild. {}",
-                    e
-                )))
-            })?;
-        let _channel = get_channel_to_send(guild).await?;
+    let full_image_path = if Path::new(full_image_path).exists() {
+        fip
     } else {
-        let fip = format!("{}/default.webp", SERVER_IMAGE_PATH);
-        let full_image_path = fip.as_str();
-        let guild = member
-            .guild_id
-            .to_partial_guild_with_counts(&ctx.http)
-            .await
-            .map_err(|e| {
-                NewMemberError(NewMemberErrorOptionError(format!(
-                    "there was an error getting the guild. {}",
-                    e
-                )))
-            })?;
-        let dim_x = 4000;
-        let dim_y = 1000;
-        let user_pfp = member.face().replace("?size=1024", "?size=128");
-        let overlay_image  = get_image_from_url(user_pfp).await?;
-        let mut overlay_image = overlay_image.to_rgba8();
-        let radius = 128 / 2;
+        format!("{}/default.webp", SERVER_IMAGE_PATH)
+    }.as_str();
+    let guild = member
+        .guild_id
+        .to_partial_guild_with_counts(&ctx.http)
+        .await
+        .map_err(|e| {
+            NewMemberError(NewMemberErrorOptionError(format!(
+                "there was an error getting the guild. {}",
+                e
+            )))
+        })?;
+    let dim_x = 4000;
+    let dim_y = 1000;
+    let user_pfp = member.face().replace("?size=1024", "?size=128");
+    let overlay_image  = get_image_from_url(user_pfp).await?;
+    let mut overlay_image = overlay_image.to_rgba8();
+    let radius = 128 / 2;
 
-        // Apply the round mask
-        for (x, y, pixel) in overlay_image.enumerate_pixels_mut() {
-            let dx = x as i32 - 128i32 / 2;
-            let dy = y as i32 - 128i32 / 2;
-            let distance = ((dx * dx + dy * dy) as f32).sqrt();
+    // Apply the round mask
+    for (x, y, pixel) in overlay_image.enumerate_pixels_mut() {
+        let dx = x as i32 - 128i32 / 2;
+        let dy = y as i32 - 128i32 / 2;
+        let distance = ((dx * dx + dy * dy) as f32).sqrt();
 
-            if distance > radius as f32 {
-                // Outside the circle, make it transparent
-                *pixel = Rgba([pixel[0], pixel[1], pixel[2], 0]);
-            }
+        if distance > radius as f32 {
+            // Outside the circle, make it transparent
+            *pixel = Rgba([pixel[0], pixel[1], pixel[2], 0]);
         }
-        let mut bg_image  = DynamicImage::new_rgba16(dim_x, dim_y);
-        let offset_x = (dim_x / 2) - (128 / 2);
-        let offset_y = (dim_y / 2) - (128 / 2);
-        imageops::overlay(&mut bg_image, &overlay_image, offset_x as i64, offset_y as i64);
-        let uuid = Uuid::new_v4();
-        let path = format!("{}.png", uuid);
-        bg_image.save(&path).map_err(|e| {
-            NewMemberError(NewMemberErrorOptionError(format!(
-                "there was an error when creating the image. {}",
-                e
-            )))
-        })?;;
-
-        let channel = get_channel_to_send(guild).await?;
-        let attachment = CreateAttachment::path(&path).await.map_err(|e| {
-            NewMemberError(NewMemberErrorOptionError(format!(
-                "there was an error sending the image. {}",
-                e
-            )))
-        })?;
-        let mut create_message = CreateMessage::default();
-        create_message = create_message.content("This is a test");
-        create_message = create_message.add_file(attachment);
-        channel.send_message(&ctx.http, create_message).await.map_err(|e| {
-            NewMemberError(NewMemberErrorOptionError(format!(
-                "there was an error sending the message. {}",
-                e
-            )))
-        })?;
-
-        fs::remove_file(path).map_err(|e| {
-            NewMemberError(NewMemberErrorOptionError(format!(
-                "Failed to remove the file. {}",
-                e
-            )))
-        })?;
     }
+    let mut bg_image  = DynamicImage::new_rgba16(dim_x, dim_y);
+    let offset_x = (dim_x / 2) - (128 / 2);
+    let offset_y = (dim_y / 2) - (128 / 2);
+    imageops::overlay(&mut bg_image, &overlay_image, offset_x as i64, offset_y as i64);
+    let uuid = Uuid::new_v4();
+    let path = format!("{}.png", uuid);
+    bg_image.save(&path).map_err(|e| {
+        NewMemberError(NewMemberErrorOptionError(format!(
+            "there was an error when creating the image. {}",
+            e
+        )))
+    })?;;
+
+    let channel = get_channel_to_send(guild).await?;
+    let attachment = CreateAttachment::path(&path).await.map_err(|e| {
+        NewMemberError(NewMemberErrorOptionError(format!(
+            "there was an error sending the image. {}",
+            e
+        )))
+    })?;
+    let mut create_message = CreateMessage::default();
+    create_message = create_message.content("This is a test");
+    create_message = create_message.add_file(attachment);
+    channel.send_message(&ctx.http, create_message).await.map_err(|e| {
+        NewMemberError(NewMemberErrorOptionError(format!(
+            "there was an error sending the message. {}",
+            e
+        )))
+    })?;
+
+    fs::remove_file(path).map_err(|e| {
+        NewMemberError(NewMemberErrorOptionError(format!(
+            "Failed to remove the file. {}",
+            e
+        )))
+    })?;
 
     Ok(())
 }
