2501e4f469756d5161b354fc4d9f5a967c526fe9
valgul <valgulnecron@gmail.com>
2024-02-05 20:45:56 +0100

    need a default.webp image and it should work.

--- src/new_member.rs
+++ src/new_member.rs
@@ -1,11 +1,12 @@
 use crate::constant::SERVER_IMAGE_PATH;
 use crate::error_enum::AppError;
-use crate::error_enum::AppError::{JoiningError, NewMemberError};
+use crate::error_enum::AppError::{DifferedError, JoiningError, NewMemberError};
 use crate::error_enum::JoiningError::FailedToCreateDirectory;
 use crate::error_enum::NewMemberError::NewMemberErrorOptionError;
-use serenity::all::{ChannelId, Context, Member, PartialGuild};
+use serenity::all::{ChannelId, Context, CreateAttachment, CreateMessage, Member, PartialGuild};
 use std::fs;
 use std::path::Path;
+use crate::error_enum::DifferedCommandError::DifferedCommandSendingError;
 
 pub async fn new_member(ctx: Context, member: &mut Member) -> Result<(), AppError> {
     if !Path::new(SERVER_IMAGE_PATH).exists() {
@@ -17,13 +18,13 @@ pub async fn new_member(ctx: Context, member: &mut Member) -> Result<(), AppErro
         })?;
     }
 
-    let fip = format!("{}/{}.png", SERVER_IMAGE_PATH, member.guild_id);
+    let fip = format!("{}/{}.webp", SERVER_IMAGE_PATH, member.guild_id);
     let full_image_path = fip.as_str();
 
     if Path::new(full_image_path).exists() {
         let guild = member
             .guild_id
-            .to_partial_guild_with_counts(ctx.http)
+            .to_partial_guild_with_counts(&ctx.http)
             .await
             .map_err(|e| {
                 NewMemberError(NewMemberErrorOptionError(format!(
@@ -33,22 +34,43 @@ pub async fn new_member(ctx: Context, member: &mut Member) -> Result<(), AppErro
             })?;
         let _channel = get_channel_to_send(guild).await?;
     } else {
-        let fip = format!("{}/default.png", SERVER_IMAGE_PATH);
-        let _full_image_path = fip.as_str();
+        let fip = format!("{}/default.webp", SERVER_IMAGE_PATH);
+        let full_image_path = fip.as_str();
+        let guild = member
+            .guild_id
+            .to_partial_guild_with_counts(&ctx.http)
+            .await
+            .map_err(|e| {
+                NewMemberError(NewMemberErrorOptionError(format!(
+                    "there was an error getting the guild. {}",
+                    e
+                )))
+            })?;
+        let channel = get_channel_to_send(guild).await?;
+        let attachment = CreateAttachment::path(full_image_path).await.map_err(|e| {
+            NewMemberError(NewMemberErrorOptionError(format!(
+                "there was an error sending the image. {}",
+                e
+            )))
+        })?;
+        let mut create_message = CreateMessage::default();
+        create_message = create_message.content("This is a test");
+        create_message = create_message.add_file(attachment);
+        channel.send_message(&ctx.http, create_message).await.map_err(|e| {
+            NewMemberError(NewMemberErrorOptionError(format!(
+                "there was an error sending the message. {}",
+                e
+            )))
+        })?;
     }
 
     Ok(())
 }
 
 async fn get_channel_to_send(guild: PartialGuild) -> Result<ChannelId, AppError> {
-    let channel_id = guild
+    guild
         .system_channel_id
         .ok_or(NewMemberError(NewMemberErrorOptionError(
             "there was an error getting the channel to send".to_string(),
-        )));
-    if let Err(ref _e) = channel_id {
-        Ok(channel_id.unwrap_or_default())
-    } else {
-        channel_id
-    }
+        )))
 }
