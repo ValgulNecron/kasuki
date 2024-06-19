4a2a7cc9279fa0f9ca115727bbe2496be5fdb48f
valgul <valgulnecron@gmail.com>
2024-06-19 20:20:19 +0200

    V2.9.3 fixed action

--- build.rs
+++ build.rs
@@ -20,25 +20,21 @@ fn main() -> Result<(), Box<dyn Error>> {
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("shard_descriptor.bin"))
         .compile(&["proto/shard.proto"], &["proto"])?;
-    tonic_build::compile_protos("proto/shard.proto")?;
 
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("info_descriptor.bin"))
         .compile(&["proto/info.proto"], &["proto"])?;
-    tonic_build::compile_protos("proto/info.proto")?;
 
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("command_descriptor.bin"))
         .compile(&["proto/command.proto"], &["proto"])?;
-    tonic_build::compile_protos("proto/command.proto")?;
 
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("federation_descriptor.bin"))
         .compile(&["proto/federation.proto"], &["proto"])?;
-    tonic_build::compile_protos("proto/federation.proto")?;
 
     cynic_codegen::register_schema("anilist")
         .from_sdl_file("schemas/anilist.graphql")?
