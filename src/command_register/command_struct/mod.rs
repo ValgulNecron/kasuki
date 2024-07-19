507d8f5b5799056e825393741ff4185163021276
valgul <valgulnecron@gmail.com>
2024-07-19 21:03:55 +0200

    V2.10.1 started working on bot management command

--- src/command_register/command_struct/mod.rs
+++ src/command_register/command_struct/mod.rs
@@ -4,3 +4,4 @@ pub mod message_command;
 pub mod subcommand;
 pub mod subcommand_group;
 pub mod user_command;
+pub mod guild_command;
--- src/command_register/registration_function/mod.rs
+++ src/command_register/registration_function/mod.rs
@@ -4,3 +4,4 @@ pub mod register_message_command;
 pub mod register_subcommand;
 pub mod register_subcommand_group;
 pub mod register_user_command;
+pub mod register_guild_specific_command;
