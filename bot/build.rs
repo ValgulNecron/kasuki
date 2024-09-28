6ce05052c5d080cd0810f07e18da48a7a70ddf64
valgul <valgulnecron@gmail.com>
2024-09-29 00:26:17 +0200

    Fix: dep update

--- bot/build.rs
+++ bot/build.rs
@@ -22,17 +22,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("shard_descriptor.bin"))
-        .compile(&["proto/shard.proto"], &["proto"])?;
+        .compile_protos(&["proto/shard.proto"], &["proto"])?;
 
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("info_descriptor.bin"))
-        .compile(&["proto/info.proto"], &["proto"])?;
+        .compile_protos(&["proto/info.proto"], &["proto"])?;
 
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("command_descriptor.bin"))
-        .compile(&["proto/command.proto"], &["proto"])?;
+        .compile_protos(&["proto/command.proto"], &["proto"])?;
 
     cynic_codegen::register_schema("anilist")
         .from_sdl_file("schemas/anilist.graphql")?
