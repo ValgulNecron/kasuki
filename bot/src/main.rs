aec478def1e1a2749515b5dc1d443cac5b44c8da
valgul <valgulnecron@gmail.com>
2024-09-29 00:57:21 +0200

    Feat: full rewrite for serenity next

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -3,15 +3,41 @@ mod event_handler;
 mod command;
 
 use anyhow::{Context, Result};
+use serenity::gateway::ActivityData;
 
 #[tokio::main]
 
-fn main() -> Result<()> {
+async fn main() -> Result<()> {
+    // launch migration
 
-    #[cfg(unix)]
-    {
-        // wait for exit signal
-    }
+
+    // get config
+
+
+    // create logger
+
+
+    // create bot client
+    let handler = ;
+
+
+    let intent = serenity::prelude::GatewayIntents::non_privileged();
+    let privileged = serenity::prelude::GatewayIntents::GUILD_PRESENCES
+        | serenity::prelude::GatewayIntents::GUILD_MEMBERS
+        | serenity::prelude::GatewayIntents::MESSAGE_CONTENT;
+
+    let all = intent | privileged;
+
+
+    let client = serenity::Client::builder(token, all)
+        .event_handler(handler)
+        .activity(ActivityData::custom(
+
+        ))
+        .await
+        .context("Failed to create client")?;
+
+    let shard_manager = client.shard_manager.clone();
 
     Ok(())
 }
