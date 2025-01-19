a3dd0cf4ea473b54fe87a64eace5dfabcc4a405c
Valgul <valgulnecron@gmail.com>
2025-01-19 17:47:52 +0100

    anisong

--- bot/src/background_task/get_anisong_db.rs
+++ bot/src/background_task/get_anisong_db.rs
@@ -1,3 +1,4 @@
+use futures::future::join_all;
 use reqwest::Client;
 use sea_orm::ActiveValue::Set;
 use sea_orm::EntityTrait;
@@ -5,11 +6,11 @@ use std::collections::HashMap;
 use std::sync::Arc;
 // Import necessary libraries and modules
 use crate::config::DbConfig;
-use crate::database::prelude::{AnimeSong, GuildSubscription};
+use crate::database::prelude::AnimeSong;
 use crate::get_url;
 use serde::Deserialize;
 use serde_json::json;
-use tokio::sync::RwLock;
+use tokio::sync::{RwLock, Semaphore};
 use tracing::error;
 use tracing::trace;
 
@@ -31,144 +32,131 @@ pub async fn get_anisong(
 	anisong_list: Arc<RwLock<HashMap<String, AniSongDB>>>, db: DbConfig,
 ) -> Arc<RwLock<HashMap<String, AniSongDB>>> {
 	let connection = match sea_orm::Database::connect(get_url(db)).await {
-		Ok(connection) => connection,
+		Ok(connection) => Arc::new(connection),
 		Err(e) => {
 			error!("Failed to connect to the database. {}", e);
-
 			return anisong_list;
 		},
 	};
-	let client = Client::new();
+
+	let client = Arc::new(Client::new());
+	let semaphore = Arc::new(Semaphore::new(10)); // Limit to 10 concurrent tasks
+	let mut futures = Vec::new();
 	let mut i = 1;
-	let mut raw_anisongs = Vec::new();
-	let mut count = 0;
-	let max_count = 100_000_000;
-	loop {
-		let response = match client
-			.post("https://anisongdb.com/api/annId_request")
-			.header("Content-Type", "application/json")
-			.header("Accept", "application/json")
-			.json(&json!({
-				"annId": i,
-				"ignore_duplicate": false,
-			}))
-			.send()
-			.await
-		{
-			Ok(res) => {
-				if count >= max_count {
-					break;
-				} else {
-					count = count + 1;
-				}
-				res
-			},
-			Err(e) => {
-				error!("Failed to get anisong db {}......... /{}", e, i);
-				if count >= max_count {
-					break;
-				} else {
-					count = count + 1;
-					continue;
-				}
-			},
-		};
-		trace!(?response);
-		let json = match response.text().await {
-			Ok(res) => {
-				if count >= max_count {
-					break;
-				} else {
-					count = count + 1;
-				}
-				res
-			},
-			Err(e) => {
-				error!("Failed to get anisong db {}", e);
-				if count >= max_count {
-					break;
-				} else {
-					count = count + 1;
-					continue;
-				}
-			},
-		};
-		trace!(?json);
-		let mut raw_anisong: Vec<RawAniSongDB> = match serde_json::from_str(&json) {
-			Ok(res) => {
-				if count >= max_count {
-					break;
-				} else {
-					count = count + 1;
-				}
-				res
-			},
-			Err(e) => {
-				error!("Failed to get anisong db {}", e);
-				if count >= max_count {
-					break;
-				} else {
-					count = count + 1;
-					continue;
-				}
-			},
-		};
+	let max_count: i64 = 100_000_000;
 
-		for anisong in raw_anisong.clone() {
-			match AnimeSong::insert(crate::database::anime_song::ActiveModel {
-				anilist_id: Set(anisong.linked_ids.anilist.to_string()),
-				ann_id: Set(anisong.ann_id.to_string()),
-				ann_song_id: Set(anisong.ann_song_id.to_string()),
-				anime_en_name: Set(anisong.anime_en_name),
-				anime_jp_name: Set(anisong.anime_jp_name),
-				anime_alt_name: Set(anisong.anime_alt_name.unwrap_or_default().join(", ")),
-				song_type: Set(anisong.song_type),
-				song_name: Set(anisong.song_name),
-				hq: Set(format!(
-					"https://files.catbox.moe/{}",
-					anisong.hq.unwrap_or_default()
-				)),
-				mq: Set(format!(
-					"https://files.catbox.moe/{}",
-					anisong.mq.unwrap_or_default()
-				)),
-				audio: Set(format!(
-					"https://files.catbox.moe/{}",
-					anisong.audio.unwrap_or_default()
-				)),
-			})
-			.on_conflict(
-				sea_orm::sea_query::OnConflict::columns([
-					crate::database::anime_song::Column::AnilistId,
-					crate::database::anime_song::Column::AnnId,
-					crate::database::anime_song::Column::AnnSongId,
-				])
-				.update_columns([
-					crate::database::anime_song::Column::AnimeEnName,
-					crate::database::anime_song::Column::AnimeJpName,
-					crate::database::anime_song::Column::AnimeAltName,
-					crate::database::anime_song::Column::SongType,
-					crate::database::anime_song::Column::SongName,
-					crate::database::anime_song::Column::Hq,
-					crate::database::anime_song::Column::Mq,
-					crate::database::anime_song::Column::Audio,
-				])
-				.to_owned(),
-			)
-			.exec(&connection)
-			.await
+	while i <= max_count {
+		let permit = semaphore.clone().acquire_owned().await.unwrap();
+		let client = client.clone();
+		let connection = connection.clone();
+
+		let future = tokio::spawn(async move {
+			let _permit = permit; // Keep permit alive for the duration of the task
+
+			let response = match client
+				.post("https://anisongdb.com/api/annId_request")
+				.header("Content-Type", "application/json")
+				.header("Accept", "application/json")
+				.json(&json!({
+					"annId": i,
+					"ignore_duplicate": false,
+				}))
+				.send()
+				.await
 			{
-				Ok(_) => {},
-				Err(e) => error!("Failed to insert anisong. {}", e),
+				Ok(res) => res,
+				Err(e) => {
+					error!("Failed to get anisong db {}......... /{}", e, i);
+					return Vec::new();
+				},
 			};
-		}
+			trace!(?i);
+			trace!(?response);
+			let json = match response.text().await {
+				Ok(res) => res,
+				Err(e) => {
+					error!("Failed to get anisong db {}", e);
+					return Vec::new();
+				},
+			};
 
-		raw_anisongs.append(&mut raw_anisong);
-		i = i + 1;
+			let raw_anisong: Vec<RawAniSongDB> = match serde_json::from_str(&json) {
+				Ok(res) => res,
+				Err(e) => {
+					error!("Failed to get anisong db {}", e);
+					return Vec::new();
+				},
+			};
+
+			for anisong in raw_anisong.clone() {
+				if anisong.linked_ids.anilist.is_none() {
+					continue;
+				}
+
+				match AnimeSong::insert(crate::database::anime_song::ActiveModel {
+					anilist_id: Set(anisong.linked_ids.anilist.unwrap().to_string()),
+					ann_id: Set(anisong.ann_id.to_string()),
+					ann_song_id: Set(anisong.ann_song_id.to_string()),
+					anime_en_name: Set(anisong.anime_en_name),
+					anime_jp_name: Set(anisong.anime_jp_name),
+					anime_alt_name: Set(anisong.anime_alt_name.unwrap_or_default().join(", ")),
+					song_type: Set(anisong.song_type),
+					song_name: Set(anisong.song_name),
+					hq: Set(format!(
+						"https://files.catbox.moe/{}",
+						anisong.hq.unwrap_or_default()
+					)),
+					mq: Set(format!(
+						"https://files.catbox.moe/{}",
+						anisong.mq.unwrap_or_default()
+					)),
+					audio: Set(format!(
+						"https://files.catbox.moe/{}",
+						anisong.audio.unwrap_or_default()
+					)),
+				})
+				.on_conflict(
+					sea_orm::sea_query::OnConflict::columns([
+						crate::database::anime_song::Column::AnilistId,
+						crate::database::anime_song::Column::AnnId,
+						crate::database::anime_song::Column::AnnSongId,
+					])
+					.update_columns([
+						crate::database::anime_song::Column::AnimeEnName,
+						crate::database::anime_song::Column::AnimeJpName,
+						crate::database::anime_song::Column::AnimeAltName,
+						crate::database::anime_song::Column::SongType,
+						crate::database::anime_song::Column::SongName,
+						crate::database::anime_song::Column::Hq,
+						crate::database::anime_song::Column::Mq,
+						crate::database::anime_song::Column::Audio,
+					])
+					.to_owned(),
+				)
+				.exec(&*connection)
+				.await
+				{
+					Ok(_) => {},
+					Err(e) => error!("Failed to insert anisong. {}", e),
+				};
+			}
+
+			raw_anisong
+		});
+
+		futures.push(future);
+		i += 1;
 	}
-
-	let new_anisong: HashMap<String, AniSongDB> = raw_anisongs
+	let results = join_all(futures).await;
+	let mut all_anisongs = Vec::new();
+	for result in results {
+		if let Ok(anisongs) = result {
+			all_anisongs.extend(anisongs);
+		}
+	}
+	let new_anisong: HashMap<String, AniSongDB> = all_anisongs
 		.into_iter()
+		.filter(|ani_song| ani_song.linked_ids.anilist.is_some())
 		.map(|ani_song| {
 			let data: AniSongDB = AniSongDB {
 				ann_id: ani_song.ann_id,
@@ -183,7 +171,10 @@ pub async fn get_anisong(
 				audio: ani_song.audio,
 			};
 
-			(ani_song.linked_ids.anilist.to_string(), data)
+			(
+				ani_song.linked_ids.anilist.unwrap_or_default().to_string(),
+				data,
+			)
 		})
 		.collect();
 
@@ -221,5 +212,5 @@ pub struct RawAniSongDB {
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Linked {
-	anilist: i64,
+	anilist: Option<i64>,
 }
