41bf7296b208bdefa4728df52e4413f1d0dc33ba
valgul <valgulnecron@gmail.com>
2025-05-08 17:55:01 +0200

    "Refactor `ListAllActivity`: Introduce `get_contents` method, streamline embed generation, and remove `run_slash`."

--- bot/src/command/anilist_server/list_all_activity.rs
+++ bot/src/command/anilist_server/list_all_activity.rs
@@ -1,4 +1,4 @@
-use crate::command::command_trait::{Command, Embed, EmbedContent, EmbedType, SlashCommand};
+use crate::command::command_trait::{Command, CommandRun, EmbedContent, EmbedType, SlashCommand};
 use crate::components::anilist::list_all_activity::get_formatted_activity_list;
 use crate::constant::ACTIVITY_LIST_LIMIT;
 use crate::database::activity_data::Column;
@@ -28,53 +28,43 @@ impl Command for ListAllActivity {
 	fn get_command_interaction(&self) -> &CommandInteraction {
 		&self.command_interaction
 	}
-}
 
-impl SlashCommand for ListAllActivity {
-	async fn run_slash(&self) -> Result<()> {
+	async fn get_contents(&self) -> Result<Vec<EmbedContent<'_, '_>>> {
 		let ctx = self.get_ctx();
 		let command_interaction = self.get_command_interaction();
 		let bot_data = ctx.data::<BotData>().clone();
 		let config = bot_data.config.clone();
+
+		self.defer().await?;
+		
 		let guild_id = command_interaction
 			.guild_id
 			.ok_or(anyhow!("Could not get the id of the guild"))?;
-
-		let list_activity_localised_text =
-			load_localization_list_activity(guild_id.to_string(), config.db.clone()).await?;
-
-		self.defer().await?;
-
+		
 		let connection = bot_data.db_connection.clone();
-
 		let list = ActivityData::find()
 			.filter(Column::ServerId.eq(guild_id.to_string()))
 			.all(&*connection)
 			.await?;
-
 		let len = list.len();
-
 		let next_page = 1;
 
 		let activity: Vec<String> = get_formatted_activity_list(list, 0);
-
 		let join_activity = activity.join("\n");
 
-		let mut content = EmbedContent::new(list_activity_localised_text.title)
+		let list_activity_localised_text =
+			load_localization_list_activity(guild_id.to_string(), config.db.clone()).await?;
+		
+		let mut embed_content = EmbedContent::new(list_activity_localised_text.title)
 			.description(join_activity)
 			.command_type(EmbedType::Followup);
-
-		trace!("{:?}", len);
-
-		trace!("{:?}", ACTIVITY_LIST_LIMIT);
-
 		if len > ACTIVITY_LIST_LIMIT as usize {
-			content = content.action_row(vec![CreateActionRow::Buttons(Cow::from(vec![
+			embed_content = embed_content.action_row(vec![CreateActionRow::Buttons(Cow::from(vec![
 				CreateButton::new(format!("next_activity_{}", next_page))
 					.label(list_activity_localised_text.next),
 			]))]);
 		}
-
-		self.send_embed(vec![content]).await
+		
+		Ok(vec![embed_content])
 	}
 }
