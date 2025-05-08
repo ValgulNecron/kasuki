f6ae2275afa8a472b72bb5e8d9bf64c44d7a8aef
valgul <valgulnecron@gmail.com>
2025-05-08 20:17:33 +0200

    "Refactor `ListRegisterUser` to enhance structure, add documentation, and implement `get_contents` method."

--- bot/src/command/anilist_server/list_register_user.rs
+++ bot/src/command/anilist_server/list_register_user.rs
@@ -1,4 +1,15 @@
-use crate::command::command_trait::{Command, Embed, EmbedContent, EmbedType, SlashCommand};
+//! `ListRegisterUser` is a structure that implements the [`Command`] trait and is responsible for
+//! listing registered users in a Discord guild, fetching their associated AniList user IDs,
+//! and displaying them via Discord Slash commands.
+//!
+//! This implementation fetches guild members, queries the associated registered users in the database,
+//! and formats the data to be shown in an embed response.
+//!
+//! # Fields
+//!
+//! * `ctx`: The [`SerenityContext`] instance, representing the Discord bot's context.
+//! * `command
+use crate::command::command_trait::{Command, CommandRun, EmbedContent, EmbedType};
 use crate::constant::MEMBER_LIST_LIMIT;
 use crate::database::prelude::RegisteredUser;
 use crate::database::registered_user::Column;
@@ -18,69 +29,100 @@ use std::borrow::Cow;
 use std::sync::Arc;
 use tracing::trace;
 
+/// A structure that encapsulates context and interaction details for listing registered users.
+///
+/// The `ListRegisterUser` struct is used to handle command interactions within the Serenity framework.
+/// It provides access to the command interaction data and the bot's context, making it useful for
+/// implementing functionality that involves listing
 pub struct ListRegisterUser {
 	pub ctx: SerenityContext,
 	pub command_interaction: CommandInteraction,
 }
 
 impl Command for ListRegisterUser {
+	/// Retrieves a reference to the `SerenityContext` associated with the current instance.
+	///
+	/// # Returns
+	/// A reference to the `SerenityContext` (`&SerenityContext`).
+	///
+	/// # Example
+	/// ```rust
+	/// let context = instance.get_ctx();
+	/// // You can now use the context to interact with the Discord API.
+	/// ```
+	///
+	/// This method provides access to
 	fn get_ctx(&self) -> &SerenityContext {
 		&self.ctx
 	}
 
+	/// Retrieves a reference to the `CommandInteraction` instance associated with the current object.
+	///
+	/// # Returns
+	/// A reference to the `CommandInteraction` instance stored within the object.
+	///
+	/// # Example
+	/// ```rust
+	///
 	fn get_command_interaction(&self) -> &CommandInteraction {
 		&self.command_interaction
 	}
-}
 
-impl SlashCommand for ListRegisterUser {
-	async fn run_slash(&self) -> Result<()> {
+	/// Asynchronously retrieves a list of `EmbedContent` with user-related information.
+	///
+	/// This function interacts with a Discord guild to fetch a list of users and their associated data.
+	/// It defers the initial response to indicate processing and fetches necessary information
+	/// like guild configuration, localization data, and user lists.
+	///
+	/// # Returns:
+	/// -
+	async fn get_contents(&self) -> Result<Vec<EmbedContent<'_, '_>>> {
 		let ctx = self.get_ctx();
 		let bot_data = ctx.data::<BotData>().clone();
 		let command_interaction = self.get_command_interaction();
 		let config = bot_data.config.clone();
 		let connection = bot_data.db_connection.clone();
 
-		let guild_id = match command_interaction.guild_id {
-			Some(id) => id.to_string(),
-			None => String::from("0"),
-		};
+		self.defer().await?;
 
-		// Load the localized text for the list user command
-		let list_user_localised = load_localization_list_user(guild_id, config.db.clone()).await?;
-
-		// Retrieve the guild from the guild ID
 		let guild_id = match command_interaction.guild_id {
 			Some(id) => id,
 			None => return Err(anyhow!("Failed to get the id of the guild")),
 		};
-
+		let list_user_localised = load_localization_list_user(guild_id.to_string(), config.db.clone()).await?;
 		let guild = guild_id.to_partial_guild_with_counts(&ctx.http).await?;
-
-		self.defer().await?;
 
 		let (desc, len, last_id): (String, usize, Option<UserId>) =
 			get_the_list(guild, ctx, None, connection).await?;
-
 		let mut content = EmbedContent::new(list_user_localised.title)
 			.description(desc)
 			.command_type(EmbedType::Followup);
-
 		if len >= MEMBER_LIST_LIMIT as usize {
 			content.action_row = vec![CreateActionRow::Buttons(Cow::from(vec![
 				CreateButton::new(format!("user_{}_0", last_id.unwrap()))
 					.label(list_user_localised.next),
 			]))];
 		}
-		self.send_embed(vec![content]).await
+
+		Ok(vec![content]).await
 	}
 }
 
+/// A struct representing a collection of data associated with a user.
+///
+/// # Fields
+/// - `user` (`User`): A structure representing the user data.
+/// - `anilist` (`String`): A string representing the AniList username or identifier corresponding to the user.
 struct Data {
 	pub user: User,
 	pub anilist: String,
 }
 
+/// Asynchronously retrieves a list of AniList-linked users from a partial guild and returns them in a formatted string.
+///
+/// # Parameters
+/// - `guild`: The partial guild object representing the guild from which to fetch members.
+/// - `ctx`: A
 pub async fn get_the_list(
 	guild: PartialGuild, ctx: &SerenityContext, last_id: Option<UserId>,
 	connection: Arc<DatabaseConnection>,
